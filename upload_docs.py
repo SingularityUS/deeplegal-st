e9d148fdf3353517a0269828558e661191740fac
Ben <111709001+SingularityUS@users.noreply.github.com>
2023-01-29 15:00:18 -0500

    Update upload_docs.py

--- upload_docs.py
+++ upload_docs.py
@@ -4,6 +4,8 @@ from google.colab import files
 import PyPDF2
 import docx2txt
 import re
+import streamlit as st
+
 
 def process_file(file_path):
     """Extract text, file name, and page number from file_path.
@@ -27,7 +29,7 @@ def process_file(file_path):
     else:
         raise Exception("Unsupported file format")
     return text, file_path, page_number
-
+'''
 def convert_to_dataframe(file_paths, usernames):
     """Convert a list of file paths to a Pandas dataframe.
 
@@ -44,12 +46,42 @@ def convert_to_dataframe(file_paths, usernames):
         data.append([text, file_path, page_number, username])
     df = pd.DataFrame(data, columns=["Text", "File Path", "Page Number", "Username"])
     return df
+'''
+def convert_to_dataframe(file_paths):
+    rows = []
+    for file_path in file_paths:
+        text, file_name, page_number = process_file(file_path)
+        rows.append([text, file_name, page_number])
+    return pd.DataFrame(rows, columns=["Text", "Document Name", "Page Number", "Username"])
 
+# Add a file uploader widget
+uploaded_file = st.file_uploader("Upload file", type=["pdf", "txt", "docx", "doc"])
+if uploaded_file:
+    # Get the file name
+    file_name = uploaded_file.name
+    file_paths = [uploaded_file]
+    df = convert_to_dataframe(file_paths)
+    st.write("Text: ", df["Text"])
+    st.write("Document Name: ", df["Document Name"])
+    st.write("Page Number: ", df["Page Number"])
+    
+    
 def main():
     uploaded_files = files.upload()
     file_paths = list(uploaded_files.keys())
     df = convert_to_dataframe(file_paths)
     print(df)
+    
+username = st.text_input("Enter your username: ")
+if uploaded_file:
+    file_name = uploaded_file.name
+    file_paths = [uploaded_file]
+    df = convert_to_dataframe(file_paths)
+    df["Username"] = username
+    st.write("Text: ", df["Text"])
+    st.write("Document Name: ", df["Document Name"])
+    st.write("Page Number: ", df["Page Number"])
+    st.write("Username: ", df["Username"])
 
 if __name__ == '__main__':
     main()
